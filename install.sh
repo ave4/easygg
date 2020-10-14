6076c8fb26821121a300915bd9b625698a573f1a
Михалыч <gtk3@inbox.ru>
2020-10-14 21:05:00 +0500

    Добавлена поддержка SubVersion

--- install.sh
+++ install.sh
@@ -11,15 +11,15 @@ fi
 
 mkdir -p "$PREFIX" || true
 mkdir -p "$DEST" || true
-install -D -m 644 "$SCRIPTPATH/git-gui.desktop" "$DEST"
-install -D -m 644 "$SCRIPTPATH/git-gui-add.desktop" "$DEST"
-install -D -m 644 "$SCRIPTPATH/git-gui-clone.desktop" "$DEST"
-install -D -m 755 "$SCRIPTPATH/git_gui" "$PREFIX"
-install -D -m 755 "$SCRIPTPATH/git_gui_add" "$PREFIX"
-install -D -m 755 "$SCRIPTPATH/git_gui_clone" "$PREFIX"
+install -D -m 644 "$SCRIPTPATH/svn-gui.desktop" "$DEST"
+install -D -m 644 "$SCRIPTPATH/svn-gui-add.desktop" "$DEST"
+install -D -m 644 "$SCRIPTPATH/svn-gui-clone.desktop" "$DEST"
+install -D -m 755 "$SCRIPTPATH/svn_gui" "$PREFIX"
+install -D -m 755 "$SCRIPTPATH/svn_gui_add" "$PREFIX"
+install -D -m 755 "$SCRIPTPATH/svn_gui_clone" "$PREFIX"
 
 if [[ `id -u` == "0" ]]; then
-sed -i 's!bash -c "$HOME/.local/bin/easygg/git_gui %d"!/opt/easygg/git_gui %d!' "/usr/share/file-manager/actions/git-gui.desktop"
-sed -i 's!bash -c "$HOME/.local/bin/easygg/git_gui_add %d %F"!/opt/easygg/git_gui_add %d %F!' "/usr/share/file-manager/actions/git-gui-add.desktop"
-sed -i 's!bash -c "$HOME/.local/bin/easygg/git_gui_clone %d"!/opt/easygg/git_gui_clone %d!' "/usr/share/file-manager/actions/git-gui-clone.desktop"
+sed -i 's!bash -c "$HOME/.local/bin/easygg/svn_gui %d"!/opt/easygg/svn_gui %d!' "/usr/share/file-manager/actions/svn-gui.desktop"
+sed -i 's!bash -c "$HOME/.local/bin/easygg/svn_gui_add %d %F"!/opt/easygg/svn_gui_add %d %F!' "/usr/share/file-manager/actions/svn-gui-add.desktop"
+sed -i 's!bash -c "$HOME/.local/bin/easygg/svn_gui_clone %d"!/opt/easygg/svn_gui_clone %d!' "/usr/share/file-manager/actions/svn-gui-clone.desktop"
 fi
